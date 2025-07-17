ad7df419c593ef15e527eb2f5a0e013c157e6de9
Aam Hermansyah <amzhermanzyah@gmail.com>
2025-07-17 18:55:40 +0700

    [UPDATE] Voice ai integration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,4 +6,4 @@ export const ENV: Environment = {
   NEXT_PUBLIC_OPENAI_URL: "https://api.openai.com"
 };
 
-export const OPENAI_KEY = "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"
+export const OPENAI_KEY = "XXXX"
