82a3efa283da8fcd908d36f6fd0752dac0030608
Aam Hermansyah <amzhermanzyah@gmail.com>
2025-07-17 18:58:18 +0700

    [UPDATE] Voice ai integration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,5 +4,5 @@ export const ENV: Environment = {
   NEXT_PUBLIC_CUSTOMER_API: "https://mimin-test.mimin.io/mimin-backend",
   NEXT_PUBLIC_CHATBOT_URL: "https://mimin-api.mimin.io/backend-gen-ai",
   NEXT_PUBLIC_OPENAI_URL: "https://api.openai.com",
-  NEXT_PUBLIC_OPENAI_KEY: "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"
+  NEXT_PUBLIC_OPENAI_KEY: ""
 };
